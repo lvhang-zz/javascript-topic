de4b1f3f0199cb9b34690b6b5e660a4dc7dc822d
sss1490486736 <1490486736@qq.com>
2019-07-09 14:53:16 +0800

    init

---
+++ testProject.js
@@ -0,0 +1,84 @@
+// const extname = (filename) => {
+//     /* TODO */
+//     console.log(filename.split("."));
+//     var filenamefibonacci.caches = filename.split(".");
+//     if(filenamefibonacci.caches.length == 1){
+//         return ''
+//     }
+//     if(filename.split(".")[filename.split(".").length - 1] && filename.split(".")[0] != '' ){
+//         return '.' +  filename.split(".")[filename.split(".").length - 1]
+//     }else {
+//         return ''
+//     }
+//
+//
+// };
+// console.log(extname("hello.jof.dsds"))
+
+// 二  记忆化斐波那契函数
+// 1 1 2 3 5 8 13 21 34
+// 1 2 3 4 5 6 7  8  9
+/*const fibonacci = (n) => {
+    if(!fibonacci.caches){
+        fibonacci.caches = [];
+        for(var i = 0;i < 10000;i++){
+            if(i == 0 || i == 1){
+                fibonacci.caches[i+1] = 1;
+            }else if(i > 2){
+                fibonacci.caches[i] = fibonacci.caches[i-1] + fibonacci.caches[i-2];
+            }
+
+        }
+    }
+
+    return fibonacci.caches[n ];
+};
+console.log(fibonacci(9));*/
+
+
+// 三
+/*完成一个 extractStr 函数，可以把一个字符串中所有的 : 到 . 的子串解析出来并且存放到一个数组当中，例如：
+extractStr('My name is:Jerry. My age is:12.') // => ['Jerry', '12']
+注意，: 和 . 之间不包含 : 和 .。也即是说，如果 ::abc..，则返回 ['abc']*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
