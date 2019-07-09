e282fc666661b9fdbb84bcdd395269bb4e08340a
sss1490486736 <1490486736@qq.com>
2019-07-09 21:46:39 +0800

    c

--- testProject.js
+++ testProject.js
@@ -131,6 +131,76 @@ const isOverlap = (rect1, rect2) => {
 };
 
 
+/**五 完成几个 DOM 元素类名操作的工具方法：
+ addClass(dom, name)：给 dom 元素添加类名 name
+ removeClass(dom, name)：把 dom 元素的类名 name 删除
+ hasClass(dom, name)：判断 dom 元素是否有类名 name
+ * */
+const addClass = (dom, name) => {
+    dom.classList.add(name)
+};
+const removeClass = (dom, name) => {
+    dom.classList.remove(name)
+};
+const hasClass = (dom, name) => {
+    return dom.classList.contains(name);
+};
+
+
+/**请你给字符串都添加上原型方法 spacify，可以让一个字符串的每个字母都多出一个空格的间隔：***/
+String.prototype.spacify = function () {
+    return this.split('').join(" ");
+};
+// console.log("2131231".spacify());
+
+
+/** 现在有一个数组存放字符串数据：
+
+ ['item1', 'item2', 'item3', 'item4', 'item5']
+ 有另外一个数组存放一组对象：
+
+ [
+ { content: 'section1', index: 0 },
+ { content: 'section2', index: 2 }
+ ]
+ 它每个对象表示的是会往原来的数组的 index 坐标插入 content 数据（index 不会重复）：
+
+ 0      1      2      3      4
+ item1  itme2  item3  item4  item5
+ ^             ^
+ |             |
+ section1     section2
+
+ 最后结果是：['section1', 'item1', 'item2', 'section2', 'item3', 'item4', 'item5']
+ 请你完成 injectSections 函数，可以达到上述的功能：
+
+ injectSections(
+ ['item1', 'item2', 'item3', 'item4', 'item5'],
+ [
+ { content: 'section1', index: 0 },
+ { content: 'section2', index: 2 }
+ ]
+ ) // => ['section1', 'item1', 'item2', 'section2', 'item3', 'item4', 'item5']**/
+const injectSections = (items, sections) => {
+   var arr = items;
+   // console.log(sections);
+   for(var i = 0;i < sections.length;i++){
+       arr.splice(sections[i].index + i,0,sections[i].content);
+   }
+   console.log(arr);
+   return arr;
+
+};
+let items = ['1','2','3','400','5',1,24,434,24,24];
+let sections = [
+    { content: 'section1', index: 0 },
+    { content: 'section2', index: 2 },
+    { content: 'section3', index: 3 }
+];
+injectSections(items,sections);
+
+
+
 
 
 
